a71d75cd67e6252ac9b5adf3ecf48c24df71a785
nikhiltambre <nikhiltambre008@gmail.com>
2026-08-06 03:37:18 +0530

    feat(query-service): manual kafka acknowledgment
    - diable auto commit in application.properties
    - add manual acknowledgment in EventConsumerService

--- WalletQueryService/src/main/java/com/query_service/WalletQueryService/service/EventConsumerService.java
+++ WalletQueryService/src/main/java/com/query_service/WalletQueryService/service/EventConsumerService.java
@@ -2,11 +2,12 @@ package com.query_service.WalletQueryService.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.query_service.WalletQueryService.dto.KafkaCdcObject;
+import org.apache.kafka.clients.consumer.internals.Acknowledgements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
+import org.springframework.kafka.support.Acknowledgment;
 @Service
 public class EventConsumerService {
     private static final Logger log = LoggerFactory.getLogger(EventConsumerService.class);
@@ -19,11 +20,12 @@ public class EventConsumerService {
     }
 
     @KafkaListener(topics = "wallet.ledger_write_db.ledger_events", groupId = "query-service-cdc")
-    public void consume(String message) {
+    public void consume(String message, Acknowledgment ack) {
         try {
             KafkaCdcObject cdc = objectMapper.readValue(message, KafkaCdcObject.class);
             if (cdc.getPayload() == null) {
                 log.warn("Received cdc event with null payload, skipping");
+                ack.acknowledge();
                 return;
             }
             String op = cdc.getPayload().getOp();
@@ -33,9 +35,11 @@ public class EventConsumerService {
                     log.info("CDC event received: op={}, accountId={}, version={}, eventType={}",
                             op, after.getAccountId(), after.getVersion(), after.getEventType());
                     readModelProjector.project(after);
+                    ack.acknowledge();
                 }
             } else {
                 log.debug("Ignoring cdc op:{}", op);
+                ack.acknowledge();
             }
         } catch (Exception e) {
             log.error("Failed to process CDC event: {}", e.getMessage(), e);
